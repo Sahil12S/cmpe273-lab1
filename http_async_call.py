ff188f9b07ba3b9474e719149ee845afd08e20aa
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-09-01 09:23:33 -0700

    async calls

---
+++ http_async_call.py
@@ -0,0 +1,31 @@
+# Import necessary modules
+import threading, time
+import requests
+
+
+# Sends requests to the URL and
+# appends Date header to the list
+def makeRequest(dates):
+    r = requests.get('https://webhook.site/0ce35f12-4cc7-4f73-af9f-149294648631')
+    dates.append(r.headers['Date'])
+    
+
+# Load output file in append mode.
+f = open('output.txt', 'a+')
+f.write('Makeing 3 Asynchronous requests . . .\n')
+
+dates = []
+
+for i in range(3):
+    t = threading.Thread(target = makeRequest, args = (dates,))
+    t.start()
+
+# Wait until all other threads are finished
+while threading.active_count() > 1:
+    continue
+
+for date in dates:
+    f.write(date + '\n')
+
+f.write('\n')
+f.close()
