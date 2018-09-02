aeb9a6ecce581560e29cdfcd8f3d9ba49f0d09ce
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-09-02 13:36:55 -0700

    corrected method of making async calls

--- http_async_call.py
+++ http_async_call.py
@@ -3,33 +3,33 @@
 # We wait for threads to finish before exiting.
 
 # Import necessary modules
-import threading, time
-import requests
+import grequests
+
+# Function to send request.
+def makeRequests(url, file):
+    reqs = [
+        grequests.get(url),
+        grequests.get(url),
+        grequests.get(url),
+    ]
+
+    results = grequests.map(reqs)
+
+    for result in results:
+        file.write(result.headers['Date'] + '\n')
 
 
-# Sends requests to the URL and
-# appends Date header to the list
-def makeRequest(dates):
-    r = requests.get('https://webhook.site/0ce35f12-4cc7-4f73-af9f-149294648631')
-    dates.append(r.headers['Date'])
-    
+def main():
+    url = 'https://webhook.site/0ce35f12-4cc7-4f73-af9f-149294648631'
+    # Load output file in append mode.
+    f = open('output.txt', 'a+')
+    f.write('Makeing 3 Asynchronous requests . . .\n')
 
-# Load output file in append mode.
-f = open('output.txt', 'a+')
-f.write('Makeing 3 Asynchronous requests . . .\n')
+    makeRequests(url, f)
 
-dates = []
+    f.write('\n')
+    f.close()
 
-for i in range(3):
-    t = threading.Thread(target = makeRequest, args = (dates,))
-    t.start()
 
-# Wait until all other threads are finished
-while threading.active_count() > 1:
-    continue
-
-for date in dates:
-    f.write(date + '\n')
-
-f.write('\n')
-f.close()
+if __name__ == '__main__':
+    main()
